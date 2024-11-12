9d8247ec0f06c391c5bed06709f3eccbc340b801
FilipPazanin <123087390+FilipPazanin@users.noreply.github.com>
2024-11-12 11:10:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ import pprint as p
 
 load_dotenv()
 
-account_sid = ""
-auth_token = ""
-api_key = ""
-api_key_secret = ""
-twiml_app_sid = ""
-twilio_number = ""
-
 app = Flask(__name__)
 
 
@@ -27,46 +20,6 @@ def home():
         'home.html',
         title="In browser calls",
     )
-
-
-@app.route('/token', methods=['GET'])
-def get_token():
-    identity = twilio_number
-    outgoing_application_sid = twiml_app_sid
-
-    access_token = AccessToken(account_sid, api_key,
-                               api_key_secret, identity=identity)
-
-    voice_grant = VoiceGrant(
-        outgoing_application_sid=outgoing_application_sid,
-        incoming_allow=True,
-    )
-    access_token.add_grant(voice_grant)
-
-    response = jsonify(
-        {'token': access_token.to_jwt().decode(), 'identity': identity})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
-
-
-@app.route('/handle_calls', methods=['POST'])
-def call():
-    p.pprint(request.form)
-    response = VoiceResponse()
-    dial = Dial(callerId=twilio_number)
-
-    if 'To' in request.form and request.form['To'] != twilio_number:
-        print('outbound call')
-        dial.number(request.form['To'])
-    else:
-        print('incoming call')
-        caller = request.form['Caller']
-        dial = Dial(callerId=caller)
-        dial.client(twilio_number)
-
-    return str(response.append(dial))
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
